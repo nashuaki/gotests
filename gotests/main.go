4b11e895f4bc64bc257147f8d5b794f995ed4fec
nashuaki <natsu.oh@gmail.com>
2024-08-31 17:09:43 +0900

    fix: init

--- gotests/main.go
+++ gotests/main.go
@@ -69,11 +69,17 @@ var (
 	nosubtests = false
 
 	// parallel is default false.
-	parallel = true
+	parallel bool
 
 	// use map instead of slice for table tests.
 	named bool
 )
+
+func init() {
+	flag.BoolVar(&nosubtests, "nosubtests", false, "disable generating tests using the Go 1.7 subtests feature")
+	flag.BoolVar(&parallel, "parallel", true, "enable generating parallel subtests using the Go 1.7 feature")
+	flag.BoolVar(&named, "named", false, "switch table tests from using slice to map (with test name for the key)")
+}
 
 func main() {
 	flag.Parse()
